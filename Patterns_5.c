d8b4eb132c98d3a1a23cace9025bec6a1cda84a9
tejasri192001 <69144520+tejasri192001@users.noreply.github.com>
2021-12-05 11:26:55 +0530

    Update Patterns_5.c

--- Patterns_5.c
+++ Patterns_5.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    printf(" Enter the size : ");
+    scanf("%d", &n);
+    printf("\n");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < 2*n; j++){
+            if(j == n-i-1 || j == n+i-1)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    for(int i = n-1; i > 0; i--){
+        for(int j = 0; j < 2*n; j++){
+            if(j == n-i || j == n+i-2)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
